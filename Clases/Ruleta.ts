34bd526ee16bed877375956173c2c773f02918af
NicolasArtemio <nicolasartemio@gmail.com>
2024-11-21 19:48:01 -0300

    actualizacion ruleta

--- Clases/Ruleta.ts
+++ Clases/Ruleta.ts
@@ -1,16 +1,31 @@
 import { JuegoSlot } from "./JuegoSlot";
+import { questionInt} from 'readline-sync';
 
 
-class Ruleta extends JuegoSlot {
-    private numeros: number;
-    private rojo: string;
-    private negro: string;
+export class Ruleta extends JuegoSlot {
+    private bola: number = 0;
+    private numerosRojos: number[] = [1, 3, 5, 7, 9, 12, 14, 16, 18, 19, 21, 23, 25, 27, 30, 32, 34, 36];
+    private numerosNegros: number[] = [2, 4, 6, 8, 10, 11, 13, 15, 17, 20, 22, 24, 26, 29, 31, 33, 35];
+    private numeroVerde: number = 0; 
 
-    constructor(nombre: string, numeros: number, rojo: string, negro: string) {
-        super(nombre);
-        this.numeros = numeros;
-        this.rojo = rojo;
-        this.negro = negro;
+constructor(){
+    super("Ruleta Smash")
+}
 
-    }
+
+
+
+
+public girarRuleta(){
+    this.bola = Math.floor(Math.random() * 37);
+
+    if (this.bola === this.numeroVerde) {
+        console.log("La bola ha caído en el número verde (0).");
+    } else if (this.numerosRojos.includes(this.bola)) {
+        console.log(`La bola ha caído en ${this.bola} rojo.`);
+    } else if (this.numerosNegros.includes(this.bola)) {
+        console.log(`La bola ha caído en ${this.bola} negro.`);
+    } 
+}
+  
 }
--- index.ts
+++ index.ts
@@ -1,7 +1,13 @@
-import { Tragamonedas } from "./Clases/tragamoneda";
+// import { Tragamonedas } from "./Clases/tragamoneda";
+
+import { Ruleta } from "./Clases/Ruleta";
 
 
-  // Ejemplo de uso
-  const tragamonedas = new Tragamonedas(3, 1); // 3 rodillos, 1 línea de pago
-  tragamonedas.generarCombinacion();
-  tragamonedas.juegoGanador(7);  // Verificamos si el valor 7 está en la combina
+//   // Ejemplo de uso
+//   const tragamonedas = new Tragamonedas(3, 1); // 3 rodillos, 1 línea de pago
+//   tragamonedas.generarCombinacion();
+//   tragamonedas.juegoGanador(7);  // Verificamos si el valor 7 está en la combina
+
+const ruleta = new Ruleta();
+
+ruleta.girarRuleta();
